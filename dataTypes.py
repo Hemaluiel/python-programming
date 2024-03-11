d1876eaab7fd8e1d019433e0b0e51b71fdfb69f8
Hemaluiel <03230070.gcbs@rub.edu.bt>
2024-03-11 12:45:42 +0600

    first commit

---
+++ dataTypes.py
@@ -0,0 +1,45 @@
+x = "hellow World" #string
+x = 50  #integer
+x = 60.5 # float
+x =  3j # complex
+x = ["geeks","for", "geeks"]  # list
+x =  ("geeks","for", "geeks") 
+x = range(10)
+x = {"name": "Suraj", "age": 24}
+x = ["geeks","for", "geeks"]
+x = frozenset ({"geeks","for", "geeks"})
+x = True
+x = b"Geeks"
+x = bytearray(4)
+x = memoryview(bytes(6))
+x= None
+
+
+my_list = []
+my_list.insert(0,  0) # first zero is array index and second zero is value
+print(my_list)
+
+my_list.append  # add a value at the end of the list.and
+print(my_list)
+
+print(len(my_list))
+
+length = len(my_list)
+
+my_list.remove(2)  # to remove a value from the list. 2 here is the value-index
+my_list.remove(my_list[0])
+print (my_list)
+
+my_list.pop()
+print(my_list) # removing the last element in the list.
+varpop = my_list.pop() # varpop is the variable
+print(varpop) # print the value as last element in the list.
+
+
+# a stack is just a list and we have to perform a set of operation - LIFO - to remove the first element
+
+
+
+
+
+
